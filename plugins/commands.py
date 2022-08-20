2b40e2b2aebccee21ad311c18a64cd15ab64f77a
pasindux <81417838+pasindux@users.noreply.github.com>
2022-08-20 23:00:31 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -3,8 +3,13 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 @Client.on_message(filters.private & filters.command(['start']))
 async def start(c:Client, m:Message):
+    fuser = m.from_user.id
+    if check_blacklist(fuser):
+        await m.reply_text("**Sorry! You are Banned!**")
+        return
+    add_chat(fuser)
     await m.reply_text(
-        text=f"**Hey 👋 {m.from_user.mention} Dear! Welcome**\n\n**First you should get an idea about what can this bot upload and what links are supported/how to send links for me by pressing /help**",
+        text=f"**Hey 👋 {m.from_user.mention} Dear! Welcome**\n\n**First you should get an idea about what can this bot upload and what links are supported/how to send links for me by pressing help**",
         reply_markup=InlineKeyboardMarkup([
           [
             InlineKeyboardButton(text="Settings", callback_data="settings") ,
